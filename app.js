41bce4c22d3ddaaa1dcb9f0f37468671ed4e88b2
balaji <balaji@flynnhealthcare.com>
2025-06-05 14:08:56 +0530

    secondcommit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.config({ path: path.join(__dirname, 'config', 'config.env') });
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(cors());
 
 connectDataBase();
 
